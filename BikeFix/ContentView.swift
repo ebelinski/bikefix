9fc371ca14ad67a7888792507f3b0c6c6d803b5f
Eugene Belinski <ebelinski@protonmail.com>
2019-08-03 15:55:25 -0500

    basic cleanup

--- BikeFix/ContentView.swift
+++ BikeFix/ContentView.swift
@@ -1,23 +1,15 @@
-//
-//  ContentView.swift
-//  BikeFix
-//
-//  Created by PSD on 8/3/19.
-//  Copyright © 2019 TapMoko LLC. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
-    }
+  var body: some View {
+    Text("Hello World")
+  }
 }
 
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
 #endif
--- BikeFix/SceneDelegate.swift
+++ BikeFix/SceneDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  SceneDelegate.swift
-//  BikeFix
-//
-//  Created by PSD on 8/3/19.
-//  Copyright © 2019 TapMoko LLC. All rights reserved.
-//
-
 import UIKit
 import SwiftUI
 
@@ -13,18 +5,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
-
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+  func scene(_ scene: UIScene,
+             willConnectTo session: UISceneSession,
+             options connectionOptions: UIScene.ConnectionOptions) {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
     // Use a UIHostingController as window root view controller
     if let windowScene = scene as? UIWindowScene {
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UIHostingController(rootView: ContentView())
-        self.window = window
-        window.makeKeyAndVisible()
+      let window = UIWindow(windowScene: windowScene)
+      window.rootViewController = UIHostingController(rootView: ContentView())
+      self.window = window
+      window.makeKeyAndVisible()
     }
   }
 
@@ -56,6 +49,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // to restore the scene back to its current state.
   }
 
-
 }
-
