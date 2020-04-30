87015e558593c4209733eb29857c64d03bb30d14
Eugene Belinski <ebelinski@protonmail.com>
2020-04-30 13:32:43 -0500

    cleanup

--- BikeFix/Map/Map.swift
+++ BikeFix/Map/Map.swift
@@ -3,42 +3,62 @@ import MapKit
 
 struct Map: View {
 
+  // MARK: - Environment
+
   @EnvironmentObject var nodeProvider: NodeProvider
+
+  // MARK: - State
 
   @State var currentlyDisplayingLocationAuthorizationRequest = false
   @State var shouldNavigateToUserLocation = false
 
+  // MARK: - Instance variables
+
   let locationManager = CLLocationManager()
+
+  // MARK: - Body view
 
   var body: some View {
     ZStack {
-      MapView(nodes: $nodeProvider.nodes,
-              currentlyDisplayingLocationAuthorizationRequest: $currentlyDisplayingLocationAuthorizationRequest,
-              shouldNavigateToUserLocation: $shouldNavigateToUserLocation)
+      map
 
-      HStack {
-        Spacer()
-        VStack {
-          Spacer()
-          Button(action: {
-            self.checkForLocationAuthorizationAndNavigateToUserLocation()
-          }) {
-            Image(systemName: "location")
-              .imageScale(.large)
-              .accessibility(label: Text("Locate Me"))
-              .padding()
-              .background(Color.softBackground)
-              .cornerRadius(5)
-              .shadow(color: Color.shadow, radius: 5)
-          }
-          .padding()
-        }
-      }
+      mapOverlays
     }
     .onAppear {
       self.shouldNavigateToUserLocation = true
     }
   }
+
+  // MARK: - Other views
+
+  var map: some View {
+    MapView(nodes: $nodeProvider.nodes,
+            currentlyDisplayingLocationAuthorizationRequest: $currentlyDisplayingLocationAuthorizationRequest,
+            shouldNavigateToUserLocation: $shouldNavigateToUserLocation)
+  }
+
+  var mapOverlays: some View {
+    HStack {
+      Spacer()
+      VStack {
+        Spacer()
+        Button(action: {
+          self.checkForLocationAuthorizationAndNavigateToUserLocation()
+        }) {
+          Image(systemName: "location")
+            .imageScale(.large)
+            .accessibility(label: Text("Locate Me"))
+            .padding()
+            .background(Color.softBackground)
+            .cornerRadius(5)
+            .shadow(color: Color.shadow, radius: 5)
+        }
+        .padding()
+      }
+    } 
+  }
+
+  // MARK: - Methods
 
   func checkForLocationAuthorizationAndNavigateToUserLocation() {
     currentlyDisplayingLocationAuthorizationRequest = false
