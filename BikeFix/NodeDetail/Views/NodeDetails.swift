d178f8a543c831270feafabc4ab148de3303aeb0
Eugene Belinski <ebelinski@protonmail.com>
2020-05-02 11:50:27 -0500

    added ability to open addresses in google/apple maps

--- BikeFix/NodeDetail/Views/NodeDetails.swift
+++ BikeFix/NodeDetail/Views/NodeDetails.swift
@@ -20,9 +20,53 @@ struct NodeDetails: View {
   var body: some View {
     NavigationView {
       Form {
-        if nodeVM.address != nil {
-          Section(header: Text("Address")) {
-            Text(nodeVM.address!)
+        Section(header: Text("Coordinates")) {
+          Text("(\(nodeVM.location.latitude), \(nodeVM.location.longitude))")
+
+          Button(action: {
+            print("google maps")
+          }) {
+            HStack {
+              Image(systemName: "map")
+                .foregroundColor(Color.bikefixPrimary)
+              Text("Open in Google Maps")
+            }
+          }
+
+          Button(action: {
+            print("apple maps")
+          }) {
+            HStack {
+              Image(systemName: "map")
+                .foregroundColor(Color.bikefixPrimary)
+              Text("Open in Apple Maps")
+            }
+          }
+        }
+
+        Section(header: Text("Address")) {
+          Text(nodeVM.address ?? "Address not available")
+
+          if nodeVM.address != nil {
+            Button(action: {
+              self.openInGoogleMaps(address: self.nodeVM.address!)
+            }) {
+              HStack {
+                Image(systemName: "map")
+                  .foregroundColor(Color.bikefixPrimary)
+                Text("Open in Google Maps")
+              }
+            }
+
+            Button(action: {
+              self.openInAppleMaps(address: self.nodeVM.address!)
+            }) {
+              HStack {
+                Image(systemName: "map")
+                  .foregroundColor(Color.bikefixPrimary)
+                Text("Open in Apple Maps")
+              }
+            }
           }
         }
 
@@ -32,6 +76,7 @@ struct NodeDetails: View {
           }
         }
       }
+      .accentColor(Color.bikefixPrimary)
       .listStyle(GroupedListStyle())
       .navigationBarTitle(Text(nodeVM.name), displayMode: .large)
       .navigationBarItems(trailing: doneButton)
@@ -54,12 +99,42 @@ struct NodeDetails: View {
 
   // MARK: - Methods
 
+  func openInGoogleMaps(address: String) {
+    guard let encodedAddress = address.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
+    guard let URL = URL(string: "https://www.google.com/maps/search/?api=1&query=\(encodedAddress)") else { return }
+    UIApplication.shared.open(URL, options: [:], completionHandler: nil)
+  }
+
+  func openInAppleMaps(address: String) {
+    CLGeocoder().geocodeAddressString(address) { placemarks, error in
+      if let error = error {
+        print(error)
+        return
+      }
+
+      guard let placemarks = placemarks else { return }
+      let geocodedPlacemark = placemarks[0]
+      let placemark = MKPlacemark(coordinate: geocodedPlacemark.location!.coordinate,
+                                  addressDictionary: geocodedPlacemark.addressDictionary! as? [String: AnyObject])
+
+      let mapItem = MKMapItem(placemark: placemark)
+      mapItem.name = geocodedPlacemark.name
+
+      let launchOptions = NSDictionary(object: MKLaunchOptionsDirectionsModeDriving,
+                                       forKey: MKLaunchOptionsDirectionsModeKey as NSCopying)
+      let currentLocationMapItem = MKMapItem.forCurrentLocation()
+      MKMapItem.openMaps(with: [currentLocationMapItem, mapItem],
+                         launchOptions: launchOptions as? [String: AnyObject])
+    }
+  }
+
 }
 
 #if DEBUG
 struct NodeDetails_Previews: PreviewProvider {
   static var previews: some View {
     NodeDetails(nodeVM: NodeViewModel(node: DummyData.shopNode))
+      .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
   }
 }
 #endif
--- BikeFix/NodeDetail/Views/NodeDetailsRawData.swift
+++ BikeFix/NodeDetail/Views/NodeDetailsRawData.swift
@@ -25,7 +25,10 @@ struct NodeDetailsRawData: View {
 #if DEBUG
 struct NodeDetailsRawData_Previews: PreviewProvider {
   static var previews: some View {
-    NodeDetailsRawData(nodeVM: NodeViewModel(node: DummyData.shopNode))
+    NavigationView {
+      NodeDetailsRawData(nodeVM: NodeViewModel(node: DummyData.shopNode))
+    }
+    .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
   }
 }
 #endif
