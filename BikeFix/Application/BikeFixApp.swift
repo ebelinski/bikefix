35921ab6e2c1dbf4f59b90d420b2053e90430042
Eugene Belinski <ebelinski@protonmail.com>
2021-01-13 21:34:51 -0600

    Replaced AppDelegate/SceneDelegate with BikeFixApp.

---
+++ BikeFix/Application/BikeFixApp.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+import UIKit
+
+@main
+struct BikeFixApp: App {
+
+  @StateObject var nodeProvider = NodeProvider()
+  @StateObject var userSettings = UserSettings()
+
+  @Environment(\.scenePhase) private var scenePhase
+
+  @SceneBuilder var body: some Scene {
+    WindowGroup {
+      Home()
+        .environmentObject(nodeProvider)
+        .environmentObject(userSettings)
+        .onAppear(perform: didAppear)
+    }
+  }
+
+  private func didAppear() {
+    Products.store.requestProducts { success, tipProducts in
+      if success {
+        Products.tipProducts = tipProducts!
+      }
+    }
+
+    UISwitch.appearance().onTintColor = UIColor.bikefixPrimaryOnWhite
+  }
+
+}
