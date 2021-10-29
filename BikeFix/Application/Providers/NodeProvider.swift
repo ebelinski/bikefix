2f02c8a0b894366183259ca67031507e6e8b1a95
Eugene Belinski <ebelinski@protonmail.com>
2021-10-28 21:19:28 -0500

    First pass at using async/await syntax for nodes network call.

--- BikeFix/Application/Providers/NodeProvider.swift
+++ BikeFix/Application/Providers/NodeProvider.swift
@@ -1,9 +1,10 @@
 import Foundation
 import MapKit
-import Combine
 import SwiftUI
 
 class NodeProvider: ObservableObject {
+
+  enum DataError: Error { case generic }
 
   // MARK: - Public properties
 
@@ -20,14 +21,41 @@ class NodeProvider: ObservableObject {
     "https://\(apiBase.rawValue)/api/interpreter"
   }
 
-  private var task: URLSessionDataTask?
-
   // MARK: - Public methods
 
   func refreshData(forRegion region: MKCoordinateRegion) {
     log.info("Region: \(region)")
     loading = true
 
+    Task {
+      do {
+        let nodes = try await getNodes(forRegion: region)
+        self.loading = false
+        self.nodes = nodes
+      } catch {
+        self.loading = false
+      }
+    }
+  }
+
+  private func getNodes(forRegion region: MKCoordinateRegion) async throws -> [Node] {
+    let url = getRequestUrl(forRegion: region)
+
+    log.info("URL: \(url)")
+
+    let (resultingData, _) = try await URLSession.shared.data(from: url)
+
+    do {
+      let response = try JSONDecoder().decode(NodeResponse.self, from: resultingData)
+      log.info("Elements count: \(response.elements.count)")
+      return response.elements
+    } catch let error {
+      log.error(error.localizedDescription)
+      throw DataError.generic
+    }
+  }
+
+  private func getRequestUrl(forRegion region: MKCoordinateRegion) -> URL {
     let southWestLat = region.southWest.latitude
     let southWestLon = region.southWest.longitude
 
@@ -38,40 +66,10 @@ class NodeProvider: ObservableObject {
     let box = "[bbox:\(southWestLat),\(southWestLon),\(northEastLat),\(northEastLon)];"
     let node = "(node[amenity=bicycle_repair_station];node[shop=bicycle];);"
     let meta = "out%20meta;"
+
     let endpoint = "\(dataEndpoint)?\(data)\(box)\(node)\(meta)"
 
-    let url = URL(string: endpoint)!
-
-    task?.cancel()
-
-    log.info("URL: \(url)")
-    task = URLSession.shared.dataTask(with: url) { data, _, error in
-      if let error = error {
-        log.error(error.localizedDescription)
-        return
-      }
-
-      guard let data = data else {
-        log.error("Error: data is nil")
-        return
-      }
-
-      do {
-        let response = try JSONDecoder().decode(NodeResponse.self, from: data)
-        log.info("Elements count: \(response.elements.count)")
-        DispatchQueue.main.async {
-          self.loading = false
-          self.nodes = response.elements
-        }
-      } catch let error {
-        log.error(error.localizedDescription)
-        DispatchQueue.main.async {
-          self.loading = false
-        }
-      }
-    }
-
-    task?.resume()
+    return URL(string: endpoint)!
   }
 
 }
