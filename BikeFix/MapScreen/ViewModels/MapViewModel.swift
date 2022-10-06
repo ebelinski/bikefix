c3d1cb51393b96078870fe7fa421005582a57f7c
Eugene Belinski <eugenemsp@gmail.com>
2022-10-06 12:05:52 -0500

    Fixed an issue where map would not navigate to user's location after user authorized location.

--- BikeFix/MapScreen/ViewModels/MapViewModel.swift
+++ BikeFix/MapScreen/ViewModels/MapViewModel.swift
@@ -1,39 +1,54 @@
 import Foundation
 import MapKit
 
-class MapViewModel: ObservableObject {
-  
+class MapViewModel: NSObject, ObservableObject {
+
   @Published var showingSettings = false
   @Published var displayingLocationAuthRequest = false
   @Published var shouldNavigateToUserLocation = false
   @Published var openedNodeVM: NodeViewModel? = nil
-  
+
   var nodeProvider: NodeProvider
-  
-  let locationManager = CLLocationManager()
-  
+
+  private lazy var locationManager: CLLocationManager = {
+    let locationManager = CLLocationManager()
+    locationManager.delegate = self
+    return locationManager
+  }()
+
   init(nodeProvider: NodeProvider) {
     self.nodeProvider = nodeProvider
   }
-  
+
   // MARK: - Public methods
-  
+
   func checkForLocationAuthorizationAndNavigateToUserLocation() {
     displayingLocationAuthRequest = false
-    
+
     if locationManager.authorizationStatus == .notDetermined {
       log.info("location authorization not determined")
       displayingLocationAuthRequest = true
       locationManager.requestWhenInUseAuthorization()
       return
     }
-    
+
     shouldNavigateToUserLocation = true
   }
-  
+
   func onNodeDetailDismiss() {
     ReviewHelper.shared.allTimeNodeDetailDismissCount += 1
     ReviewHelper.shared.requestReviewIfAppropriate()
   }
-  
+
+}
+
+extension MapViewModel: CLLocationManagerDelegate {
+  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    switch locationManager.authorizationStatus {
+    case .authorizedAlways, .authorizedWhenInUse:
+      displayingLocationAuthRequest = false
+    default:
+      break
+    }
+  }
 }
