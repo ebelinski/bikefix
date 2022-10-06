a1c949182d0095c88d00f351809dc4ffb996d709
Eugene Belinski <eugenemsp@gmail.com>
2022-10-06 11:57:48 -0500

    Fixed file indentation.

--- BikeFix/MapScreen/ViewModels/MapViewModel.swift
+++ BikeFix/MapScreen/ViewModels/MapViewModel.swift
@@ -2,38 +2,38 @@ import Foundation
 import MapKit
 
 class MapViewModel: ObservableObject {
-
-    @Published var showingSettings = false
-    @Published var displayingLocationAuthRequest = false
-    @Published var shouldNavigateToUserLocation = false
-    @Published var openedNodeVM: NodeViewModel? = nil
-
-    var nodeProvider: NodeProvider
-
-    let locationManager = CLLocationManager()
-
-    init(nodeProvider: NodeProvider) {
-        self.nodeProvider = nodeProvider
+  
+  @Published var showingSettings = false
+  @Published var displayingLocationAuthRequest = false
+  @Published var shouldNavigateToUserLocation = false
+  @Published var openedNodeVM: NodeViewModel? = nil
+  
+  var nodeProvider: NodeProvider
+  
+  let locationManager = CLLocationManager()
+  
+  init(nodeProvider: NodeProvider) {
+    self.nodeProvider = nodeProvider
+  }
+  
+  // MARK: - Public methods
+  
+  func checkForLocationAuthorizationAndNavigateToUserLocation() {
+    displayingLocationAuthRequest = false
+    
+    if locationManager.authorizationStatus == .notDetermined {
+      log.info("location authorization not determined")
+      displayingLocationAuthRequest = true
+      locationManager.requestWhenInUseAuthorization()
+      return
     }
-
-    // MARK: - Public methods
-
-    func checkForLocationAuthorizationAndNavigateToUserLocation() {
-      displayingLocationAuthRequest = false
-
-      if locationManager.authorizationStatus == .notDetermined {
-        log.info("location authorization not determined")
-        displayingLocationAuthRequest = true
-        locationManager.requestWhenInUseAuthorization()
-        return
-      }
-
-      shouldNavigateToUserLocation = true
-    }
-
-    func onNodeDetailDismiss() {
-      ReviewHelper.shared.allTimeNodeDetailDismissCount += 1
-      ReviewHelper.shared.requestReviewIfAppropriate()
-    }
-
+    
+    shouldNavigateToUserLocation = true
+  }
+  
+  func onNodeDetailDismiss() {
+    ReviewHelper.shared.allTimeNodeDetailDismissCount += 1
+    ReviewHelper.shared.requestReviewIfAppropriate()
+  }
+  
 }
